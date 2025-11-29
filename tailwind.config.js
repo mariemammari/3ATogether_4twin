047ac709d98ee5e60cb1da0f18578fa86ba983c4
Mariem_Ammari <ammarim073@gmail.com>
2025-11-30 00:29:50 +0100

    Test_Init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./public/**/*.html", 
+    "./*.html",
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
